46d58fbe420b86e4b1d760a28c8f3ed8f90b9c13
OliverSolberg <oliver.solberg@instech.no>
2025-04-25 12:22:38 +0200

    CarsApi tested and is working.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,27 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<CarDb>(opt => opt.UseInMemoryDatabase("CarList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "CarAPI";
+    config.Title = "CarAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+if(app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "CarAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 app.MapGet("/cars", async (CarDb db) =>
     await db.Cars.ToListAsync());
